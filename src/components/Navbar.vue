c355b82fc7e7a92e96f1f1af5256f5396c120f88
Lucas <lucas98fernando@gmail.com>
2021-02-12 09:48:29 -0300

    Adding navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,38 +9,21 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item href="#">Início</b-nav-item>
+          <b-nav-item href="#">Conta digital</b-nav-item>
+          <b-nav-item href="#">Cartão de crédito</b-nav-item>
+          <b-nav-item href="#">Pix <b-badge>novo</b-badge></b-nav-item>
+          <b-nav-item href="#">Empréstimo</b-nav-item>
+          <b-nav-item href="#">Conta PJ</b-nav-item>
+          <b-nav-item href="#">Rewards</b-nav-item>
+          <b-nav-item href="#">Seguros <b-badge>novo</b-badge></b-nav-item>
+          <b-nav-item href="#">Sobre nós</b-nav-item>
+          <b-nav-item href="#">Carreiras</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input
-              size="sm"
-              class="mr-sm-2"
-              placeholder="Search"
-            ></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit"
-              >Search</b-button
-            >
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
+          <b-nav-item href="#">Login</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -59,4 +42,31 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+
+.navbar {
+  padding: 0.6rem 2rem !important;
+
+  .nav-link {
+    font-size: 1.1rem;
+    font-weight: 500;
+    color: rgba(17, 17, 17, 0.7) !important;
+    padding-right: 0.8rem !important;
+    padding-left: 0.8rem !important;
+
+    &:hover {
+      color: $purpleLight !important;
+    }
+
+    .badge {
+      border-radius: 20px;
+      background-color: $whiteSecondary;
+      color: $black;
+      font-weight: 500;
+      font-size: 1rem
+    }
+  }
+}
+@import 'node_modules/bootstrap/scss/bootstrap.scss';
+</style>
